e5fb187bbc0ce9b037ef227b0600d02b1a897e15
Pablo Di Marco <pablodimarco7@gmail.com>
2023-03-11 17:34:16 -0300

    Diseño base de la web

---
+++ animacion.js
@@ -0,0 +1,27 @@
+const desplazamiento = document.querySelector("#desplazamiento");
+
+function frames() {
+
+    const anim = desplazamiento.animate([
+        {transform: "TranslateY(0px)"},
+        {transform: `TranslateY(${screen.width > 475 ? -100 : -75}px)`}
+        ], {
+        easing: "linear",
+        iterations: 1,
+        duration: 800
+        });
+
+    return anim.finished;
+}
+
+function displace() {
+    
+    frames().then(()=> {
+       desplazamiento.appendChild(document.querySelectorAll("#desplazamiento > li")[0]);
+    })
+}
+
+
+setInterval(() => {
+    displace();
+}, 5000);
