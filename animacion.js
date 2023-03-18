565179d259e5b803c1bb602685a9f4656f5c4933
Pablo Di Marco <pablodimarco7@gmail.com>
2023-03-18 12:32:55 -0300

    Cambios para empezar con mobile first

--- animacion.js
+++ animacion.js
@@ -3,25 +3,25 @@ const desplazamiento = document.querySelector("#desplazamiento");
 function frames() {
 
     const anim = desplazamiento.animate([
-        {transform: "TranslateY(0px)"},
-        {transform: `TranslateY(${screen.width > 475 ? -100 : -75}px)`}
-        ], {
+        { transform: "TranslateY(0px)" },
+        { transform: `TranslateY(${screen.width > 475 ? -16 : -16}vw)`},
+    ], {
         easing: "linear",
         iterations: 1,
-        duration: 800
-        });
+        duration: 800,
+    });
 
     return anim.finished;
 }
 
 function displace() {
-    
-    frames().then(()=> {
-       desplazamiento.appendChild(document.querySelectorAll("#desplazamiento > li")[0]);
+
+    frames().then(() => {
+        desplazamiento.appendChild(document.querySelectorAll("#desplazamiento > li")[0]);
     })
 }
 
 
 setInterval(() => {
     displace();
-}, 5000);
+}, 5000);
